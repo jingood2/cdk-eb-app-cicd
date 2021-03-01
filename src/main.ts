965e75d038cfbfe5775e1e6471fb452bd0ffd60d
Jinyoung Kim <jingood2@Jinyoungui-MacBookAir.local>
2021-03-02 01:00:32 +0900

    Add PipelineStack

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { App, Construct, Stack, StackProps } from '@aws-cdk/core';
+import { PipelineStack } from './lib/pipeline-stack';
 
 export class MyStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps = {}) {
@@ -16,7 +17,7 @@ const devEnv = {
 
 const app = new App();
 
-new MyStack(app, 'my-stack-dev', { env: devEnv });
+new PipelineStack(app, 'CdkEBPipelineStack', { env: devEnv });
 // new MyStack(app, 'my-stack-prod', { env: prodEnv });
 
 app.synth();
