1943c2ff45beaf6f85def3f0c011b3b11483d3b2
Jinyoung Kim <jingood2@Jinyoungui-MacBookAir.local>
2021-03-04 02:35:37 +0900

    Update

--- src/lib/beanstalk-stack.ts
+++ src/lib/beanstalk-stack.ts
@@ -129,7 +129,7 @@ export class BeanstalkStack extends cdk.Stack {
           post_build: {
             commands: [
               `eb init ${envVars.APP_NAME} --region ${envVars.REGION} --platform Tomcat`,
-              `eb deploy ${envVars.APP_STAGE_NAME}`,
+              'eb deploy',
             ],
           },
         },
