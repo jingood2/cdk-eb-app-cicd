8453d7d028efa10d69843ded1e61913646dba0de
Jinyoung Kim <jingood2@Jinyoungui-MacBookAir.local>
2021-03-06 19:29:39 +0900

    Update

--- src/lib/beanstalk-stack.ts
+++ src/lib/beanstalk-stack.ts
@@ -118,7 +118,7 @@ export class BeanstalkStack extends cdk.Stack {
               'echo Installing awscli',
               'curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/tmp/awscliv2.zip"',
               'unzip /tmp/awscliv2.zip',
-              'sudo /tmp/aws/install',
+              '/tmp/aws/install',
             ],
           },
 
